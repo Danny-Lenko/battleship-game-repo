8ba4cc3ea93700206037d887dd63f3c55fc1fead
DannyLenk <danylenko.14@gmail.com>
2021-12-06 17:12:51 +0200

    updated battleship.js
    begin building the controller

--- battleship.js
+++ battleship.js
@@ -14,3 +14,50 @@ let view = {
    }
 };
 
+let model = {
+   boardSize: 7,
+   shipSize: 3,
+   shipsNumber: 3,
+   shipsSunk: 0,
+   ships: [
+      { locations: ['02', '03', '04'], hits: ['', '', ''] },
+      { locations: ['50', '51', '52'], hits: ['', '', ''] },
+      { locations: ['45', '55', '65'], hits: ['', '', ''] }
+   ],
+
+   fire: function(guess) {
+      for (let i = 0; i < this.shipsNumber; i++) {
+         let ship = this.ships[i];
+         let index = ship.locations.indexOf(guess);
+         if (index >= 0) {
+            ship.hits[index] = 'hit';
+            view.displayHit(guess);
+            this.isSunk(ship);
+            console.log(this.shipsSunk);
+            return true;
+         }
+      }
+      view.displayMiss(guess);
+      return false;
+   },
+
+   isSunk: function(ship) {
+      for (let i = 0; i < this.shipSize; i++) {
+         if (ship.hits[i] !== 'hit') {
+            return false;
+         }
+      }
+      this.shipsSunk++;
+      view.displayMessage("You sank my battleship!");
+      return true;
+   }
+};
+
+let controller = {
+   guesses: 0,
+
+   processGuess: function(guess) {
+
+   }
+}
+
