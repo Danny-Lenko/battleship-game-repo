9e98490f012477365d4cde823fc3840914e375ae
DannyLenk <danylenko.14@gmail.com>
2021-12-06 10:06:01 +0200

    updated battleship.js
    built parseGuess function

--- battleship.js
+++ battleship.js
@@ -17,8 +17,8 @@ let view = {
 let model = {
    playgroundSize: 7,
    shipSize: 3,
-   shipNumber: 3,
-   shipSunk: 0,
+   shipsNumber: 3,
+   shipsSunk: 0,
    ships: [
       { locations: ["34", "44", "54"], hits: ["", "", ""] },
       { locations: ["00", "01", "02"], hits: ["", "", ""] },
@@ -26,15 +26,15 @@ let model = {
    ],
 
    fire: function(guess) {
-      this.guesses++;
-      for (let i = 0; i < this.shipNumber; i++) {
+
+      for (let i = 0; i < this.shipsNumber; i++) {
          let ship = this.ships[i];
          let index = ship.locations.indexOf(guess);
          if (index >= 0) {
             ship.hits[index] = "hit";
             if (this.isSunk) {
                view.showMessage("You sank my battleship!");
-               this.shipSunk++;
+               this.shipsSunk++;
             }
             view.renderHit(guess);
             return true;
@@ -59,10 +59,21 @@ let controller = {
    guesses: 0,
 
    processGuess: function(guess) {
-      guess.[0]
+      let location = parseGuess(guess);
+      if (location) {
+         this.guesses++;
+         let hit = model.fire(location);
+         if (hit && model.shipsSunk === model.shipsNumber) {
+            view.showMessage("You've sunk all my battleships in " + this.guesses + " guesses");
+         }
+      }
    }
 
 };
+
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("C4");
 
 function parseGuess(guess) {
    let lettersArr = ["A", "B", "C", "D", "E", "F", "G"];
@@ -70,37 +81,37 @@ function parseGuess(guess) {
    if (guess === null || guess.length !== 2) {
       alert("Oops, please enter a letter and a number on the board");
    } else {
-      let firstChar = guess.charAt(0);
-      let row = lettersArr.indexOf(firstChar);
-      let column = guess.charAt(1);
+      const firstChar = guess.charAt(0);
+      const row = lettersArr.indexOf(firstChar);
+      const column = guess.charAt(1);
 
       if ( isNaN(row) || isNaN(column) ) {
          alert("Oops, that is not on the board");
       } else if (row < 0 || row >= model.playgroundSize 
                  || column < 0 || column >= model.playgroundSize) {
          alert("Oops, that's off the board!");
+      } else {
+         return row + column;
       }
    }
-   
-}
-
-function init() {
-   const fireButton = document.querySelector('#fireButton');
-   fireButton.addEventListener('click', function() {
-      console.log("btnClicked");
-   });
-
+   return null;
 }
 
 
 
 
+// function init() {
+//    const fireButton = document.querySelector('#fireButton');
+//    fireButton.addEventListener('click', function() {
+//       console.log("btnClicked");
+//    });
+
+// }
 
 
-model.fire("34");
-model.fire("02");
-model.fire("45");
-model.fire("56");
+
+
+
 
 
 
