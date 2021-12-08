e2a9efffeb88cf6e39201b92faf47f793b3f0453
DannyLenk <danylenko.14@gmail.com>
2021-12-08 15:32:29 +0200

    updated battleship.js
    before building the button functionality

--- battleship.js
+++ battleship.js
@@ -17,7 +17,7 @@ let view = {
 let model = {
    boardSize: 7,
    shipsNum: 3,
-   shipsSize: 3,
+   shipSize: 3,
    shipsSunk: 0,
    ships: [
       { locations: ['00', '01', '02'], hits: ['', '', ''] },
@@ -53,9 +53,41 @@ let model = {
 
 };
 
-model.fire('00');
-model.fire('01');
-// model.fire('02');
+let controller = {
+   guesses: 0,
 
-// model.fire('03');
+   processGuess: function(guess) {
+      let location = parseGuess(guess);
+      if (location) {
+         this.guesses++;
+         let hit = model.fire(location);
+         if (hit && model.shipsSunk == model.shipsNum) {
+            view.showMessage("You sank all my battleships in " + this.guesses + " guesses");
+         }
+      }
+   }
+};
+
+function parseGuess(guess) {
+   let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+
+   if (guess === null || guess.length !== 2) {
+      alert("Oops, enter a letter and a number on the board");
+   } else {
+      let firstLetter = guess.charAt(0);
+      let row = alphabet.indexOf(firstLetter);
+      let col = guess.charAt(1);
+      if (isNaN(row) || isNaN(col)) {
+         alert("Oops, that's not on the board");
+      } else if (row < 0 || row >= model.boardSize 
+              || col < 0 || col >= model.boardSize ) {
+         alert("Oops, that's off the board");
+      } else {
+         return row + col;
+      }
+   } 
+   return null;
+}
+
 
